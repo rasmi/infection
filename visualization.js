b3cbda652b3084cc8becc4f22c74d8c031306b50
Rasmi Elasmar <rrelasmar@gmail.com>
2015-11-16 18:43:12 -0500

    Added basic total_infection functionality.

--- visualization.js
+++ visualization.js
@@ -3,7 +3,6 @@ var graph;
 
 d3.json(dataurl).on('load', function(json) {
     graph = json;
-    console.log(graph);
     draw();
 }).get();
 
@@ -15,6 +14,7 @@ var draw = function() {
         .attr('width', width)
         .attr('height', height);
 
+    // Create a decent-sized force layout.
     var force = d3.layout.force()
         .nodes(graph.nodes)
         .links(graph.links)
@@ -23,24 +23,53 @@ var draw = function() {
         .gravity(0)
         .start();
 
-    var link = svg.selectAll('.link')
+    // Draw links.
+    var links = svg.selectAll('.link')
         .data(graph.links).enter()
         .append('line')
         .attr('class', 'link');
 
-    var node = svg.selectAll('.node')
+    // Draw nodes.
+    var nodes = svg.selectAll('.node')
         .data(graph.nodes).enter()
         .append('circle')
         .attr('class', 'node')
-        .attr('r', 5);
+        .attr('r', 5)
+        .call(force.drag);
+
+    var click = function(node) {
+        console.log('INFECTING!');
+        total_infection(node);
+    }
+
+    nodes.on('click', click);
 
     force.on('tick', function() {
-        link.attr('x1', function(d) { return d.source.x; })
+        links.attr('x1', function(d) { return d.source.x; })
             .attr('y1', function(d) { return d.source.y; })
             .attr('x2', function(d) { return d.target.x; })
             .attr('y2', function(d) { return d.target.y; });
 
-        node.attr('cx', function(d) { return d.x; })
+        nodes.attr('cx', function(d) { return d.x; })
             .attr('cy', function(d) { return d.y; });
+
+        nodes.classed('infected', function(d) { return d.infected; });
     });
+
+    // Build up arrays of neighboring nodes for each node.
+    links.each(function(l) {
+        var source = l.source;
+        var target = l.target;
+        (source.neighbors || (source.neighbors = [])).push(target);
+        (target.neighbors || (target.neighbors = [])).push(source);
+    });
+
+    // Recursively infects a node and all its neighbors.
+    var total_infection = function(node) {
+        if (!node.infected) {
+            node.infected = true
+            node.neighbors.forEach(function(d) { total_infection(d); });
+        };
+    }
+
 }
